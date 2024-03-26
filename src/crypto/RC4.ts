15af5d3e48306f9add946974b65fed5f6c2a06c6
Martijn Reuvers <martijn@twotribes.com>
2024-03-26 11:53:15 +0100

    Slight better readable

--- src/crypto/RC4.ts
+++ src/crypto/RC4.ts
@@ -1,19 +1,18 @@
 export function RC4(data: Buffer, key: Buffer): Buffer {
 	let x = 0;
-	let box = Array.from({ length: 256 }, (_, i) => i);
+	const box = Array.from({ length: 256 }, (_, i) => i);
+
 	for (let i = 0; i < 256; i++) {
 		x = (x + box[i] + key[i % key.length]) & 255;
 		[box[i], box[x]] = [box[x], box[i]];
 	}
-	let out: number[] = [];
 	let y = 0;
 	x = 0;
-	data.forEach((char) => {
+	const out: Uint8Array = data.map((e) => {
 		x = (x + 1) & 255;
 		y = (y + box[x]) & 255;
 		[box[x], box[y]] = [box[y], box[x]];
-		out.push(char ^ box[(box[x] + box[y]) & 255]);
+		return e ^ box[(box[x] + box[y]) & 255];
 	});
-
 	return Buffer.from(out);
 }
